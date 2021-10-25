faa2bae1d235424d5ed61a240f089b5e83d0eb20
ivhak <hakonseniver@yahoo.no>
2021-10-25 15:21:00 +0200

    cleanup

--- main.c
+++ main.c
@@ -3,19 +3,22 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdbool.h>
-
 #include "segmentlcd.h"
 
 #include "usart.h"
 
-const uint8_t NOTE_OFF = 0x80;
-const uint8_t NOTE_ON  = 0x90;
-const uint8_t MSB_MASK = 0xF0;
-const uint8_t LSB_MASK = 0x0F;
+const uint8_t MSB_MASK    = 0xF0;
+const uint8_t LSB_MASK    = 0x0F;
 const uint8_t STATUS_MASK = 0x80;
+
+typedef enum {
+    NOTE_OFF   = 0x80,
+    NOTE_ON    = 0x90,
+} Command;
 
 static inline bool is_status_byte(uint8_t byte);
 static inline bool is_data_byte(uint8_t byte);
+static inline bool is_command(uint8_t byte, Command c);
 
 typedef struct { char s[2]; } Note;
 const Note notes[12] = {
@@ -28,37 +31,36 @@ const Note notes[12] = {
     {" B"},
 };
 static Note get_note(uint8_t byte);
+
 static int get_octave(uint8_t byte);
 
 int main(void) {
     uart_init();
     SegmentLCD_Init(false);
 
-    SegmentLCD_Write("HELLO");
     __enable_irq();
 
     volatile uint8_t byte;
     while(1) {
         byte = uart_next_byte();
-        if (byte == 0) continue;
 
-        if (is_status_byte(byte)) /* Status byte */ {
-            char out[16];
-            if ((byte & MSB_MASK) == NOTE_OFF){
+        if (is_status_byte(byte)) {
+            if (is_command(byte, NOTE_OFF)) {
                 /* turn note off*/
+                char out[16];
                 uint8_t key = uart_next_nonzero_byte();
-                uint8_t vel = uart_next_byte();
-                sprintf(out, "X %s %d", get_note(key).s, get_octave(key));
-
-
-            } else if  ((byte & MSB_MASK) == NOTE_ON) {
+                sprintf(out, "X %s %d", (get_note(key)).s, get_octave(key));
+                SegmentLCD_Write((const char *)out);
+                (void) uart_next_byte();
+            } else if (is_command(byte, NOTE_ON)) {
                 /* turn note on*/
+                char out[16];
                 uint8_t key = uart_next_nonzero_byte();
-                uint8_t vel = uart_next_byte();
-
-                sprintf(out, "O %s %d", get_note(key).s, get_octave(key));
+                sprintf(out, "O %s %d", (get_note(key)).s, get_octave(key));
+                SegmentLCD_Write("");
+                SegmentLCD_Write((const char *)out);
+                (void) uart_next_byte();
             }
-            SegmentLCD_Write((const char *)out);
         }
 
     }
@@ -76,12 +78,17 @@ static inline bool is_data(uint8_t byte)
 
 }
 
+static inline bool is_command(uint8_t byte, Command c)
+{
+    return (byte & MSB_MASK) == c;
+}
+
 static Note get_note(uint8_t byte)
 {
-    return notes[(byte & LSB_MASK) % 12];
+    return notes[(byte % 12)];
 }
 
 static int get_octave(uint8_t byte)
 {
-    return (byte - (byte % 12)) / 12;
+    return ((byte - (byte%12)) / 12);
 }
