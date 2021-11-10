5fbd94441e1c5d5c3fab55487cb4348a439321a5
Oskar Sveinsen <osvein@users.noreply.github.com>
2021-11-10 13:28:49 +0100

    fix buffer overflow in set_envelope

--- synth.c
+++ synth.c
@@ -11,7 +11,7 @@ static void set_envelope(EnvelopeStep *dst, int dstlen, EnvelopeStep *src, int s
     for (EnvelopeStep *end = src + srclen; src < end; src++, dst++) *dst = *src;
 
     /* pad remaining dst steps with {0, 0} */
-    for (EnvelopeStep *end = dst + dstlen; dst < end; dst++) *dst = (EnvelopeStep){0, 0};
+    for (EnvelopeStep *end = dst + (dstlen - srclen); dst < end; dst++) *dst = (EnvelopeStep){0, 0};
 }
 
 void wavegen_set_vol_envelope(WaveGen *self, EnvelopeStep *steps, int nsteps) {
