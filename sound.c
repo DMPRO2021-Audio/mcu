3395b00b21e69102bb516f957f1cdeeaab227bc7
Andreas K. Berg <andreaskb98@gmail.com>
2021-11-22 10:44:22 +0100

    Add reverb presets and reverb field in Program

--- sound.c
+++ sound.c
@@ -5,14 +5,14 @@
 #include "sound.h"
 
 static const EnvelopeStep piano_press_envelope[] = {
-    { .rate = 130, .duration = 255, },
-    { .rate = -60, .duration = 100, },
-    { .rate = -20, .duration = 255, },
-    { .rate = -14,  .duration = 255, },
+    { .rate = 700, .duration = 180, },
+    { .rate = -140, .duration = 255, },
+    { .rate = -45, .duration = 255, },
+    { .rate = -20,  .duration = 255, },
     { .rate = -10,  .duration = 255, },
-    { .rate = -8,  .duration = 255, },
-    { .rate = -7,  .duration = 255, },
-    { .rate = -6,  .duration = 255, },
+    { .rate = -5,  .duration = 255, },
+    { .rate = -0,  .duration = 255, },
+    { .rate = -5,  .duration = 255, },
 };
 
 static const EnvelopeStep organ_press_envelope[] = {
@@ -59,16 +59,67 @@ static const EnvelopeStep tremolo_release_envelope[] = {
     { .rate = -200, .duration = 50, },
 };
 
+const Reverb reverb_presets[REVERB_PRESET_LEN] = {
+    { /* Off */
+        .tau = {1024, 1024, 1024, 1024, 1024, 1024},
+        .gain = {
+            F2FP(0.0),
+            F2FP(0.0),
+            F2FP(0.0),
+            F2FP(0.0),
+            F2FP(0.0),
+            F2FP(0.0),
+            F2FP(0.0)
+        }
+    },
+    { /* Small room, 1 sek reverb time */
+        .tau = {782 , 931, 1056, 1181, 241, 73},
+        .gain = {
+            F2FP(0.893),
+            F2FP(0.874),
+            F2FP(0.858),
+            F2FP(0.844),
+            F2FP(0.7),
+            F2FP(0.7),
+            F2FP(0.5)
+        }
+    },
+    { /* Large room, 2 sek reverb time */
+        .tau = {1441 , 1683, 1921, 2159, 241, 83},
+        .gain = {
+            F2FP(0.899),
+            F2FP(0.886),
+            F2FP(0.871),
+            F2FP(0.856),
+            F2FP(0.7),
+            F2FP(0.7),
+            F2FP(0.6)
+        }
+    },
+    { /* Hall, 4 sek reverb time */
+        .tau = {3003, 3403, 3905, 4495, 241, 83},
+        .gain = {
+            F2FP(0.895),
+            F2FP(0.883),
+            F2FP(0.867),
+            F2FP(0.853),
+            F2FP(0.7),
+            F2FP(0.7),
+            F2FP(0.7)
+        }
+    },
+};
+
 const Program programs[] = {
-    {WAVEGEN_SHAPE_PIANO, piano_press_envelope, release_envelope},
-    {WAVEGEN_SHAPE_SAWTOOTH, piano_press_envelope, release_envelope},
-    {WAVEGEN_SHAPE_SQUARE, piano_press_envelope, release_envelope},
-    {WAVEGEN_SHAPE_SINE, piano_press_envelope, release_envelope},
-    {WAVEGEN_SHAPE_PIANO, organ_press_envelope, release_envelope},
-    {WAVEGEN_SHAPE_SAWTOOTH, organ_press_envelope, release_envelope},
-    {WAVEGEN_SHAPE_SQUARE, organ_press_envelope, release_envelope},
-    {WAVEGEN_SHAPE_SINE, organ_press_envelope, release_envelope},
-    {WAVEGEN_SHAPE_SAWTOOTH, tremolo_press_envelope, tremolo_release_envelope},
+    {WAVEGEN_SHAPE_PIANO, piano_press_envelope, release_envelope, REVERB_PRESET_LARGE_ROOM},
+    {WAVEGEN_SHAPE_SAWTOOTH, piano_press_envelope, release_envelope, REVERB_PRESET_SMALL_ROOM},
+    {WAVEGEN_SHAPE_SQUARE, piano_press_envelope, release_envelope, REVERB_PRESET_LARGE_ROOM},
+    {WAVEGEN_SHAPE_SINE, piano_press_envelope, release_envelope, REVERB_PRESET_LARGE_ROOM},
+    {WAVEGEN_SHAPE_PIANO, organ_press_envelope, release_envelope, REVERB_PRESET_HALL},
+    {WAVEGEN_SHAPE_SAWTOOTH, organ_press_envelope, release_envelope, REVERB_PRESET_OFF},
+    {WAVEGEN_SHAPE_SQUARE, organ_press_envelope, release_envelope, REVERB_PRESET_OFF},
+    {WAVEGEN_SHAPE_SINE, organ_press_envelope, release_envelope, REVERB_PRESET_OFF},
+    {WAVEGEN_SHAPE_SAWTOOTH, tremolo_press_envelope, tremolo_release_envelope, REVERB_PRESET_OFF},
 };
 
 const int num_programs = lenof(programs);
--- sound.h
+++ sound.h
@@ -4,10 +4,20 @@
 #define CONCERT_PITCH_NOTE 69
 #define OCTAVE 12
 
+#define REVERB_PRESET_LEN 4
+#define REVERB_PRESET_OFF 0
+#define REVERB_PRESET_SMALL_ROOM 1
+#define REVERB_PRESET_LARGE_ROOM 2
+#define REVERB_PRESET_HALL 3
+
+int currrent_reverb_preset;
+extern const Reverb reverb_presets[REVERB_PRESET_LEN];
+
 typedef struct {
     uint8_t shape;
     const EnvelopeStep *press_envelope;
     const EnvelopeStep *release_envelope;
+    uint8_t reverb;
 } Program;
 
 extern const Program programs[];
